a2f655c06e16c7fbad366fc0d3213f0ac46ac727
Crowasang <115343930+Crowasang@users.noreply.github.com>
2023-03-28 13:23:08 +0900

    Add files via upload

---
+++ sec02/ex02/MemberServlet.java
@@ -0,0 +1,85 @@
+package sec02.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MemberServlet
+ */
+@WebServlet("/member4")
+public class MemberServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public MemberServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		MemberDAO dao = new MemberDAO();
+		List<MemberVO> list = dao.listMembers();
+		String command = request.getParameter("command");
+		
+		if (command != null && command.equals("addMember")) {
+			String _id = request.getParameter("id");
+			String _pwd = request.getParameter("pwd");
+			String _name = request.getParameter("name");
+			String _email = request.getParameter("email");
+			MemberVO vo = new MemberVO();
+			vo.setId(_id);
+			vo.setPwd(_pwd);
+			vo.setName(_name);
+			vo.setEmail(_email);
+			dao.addMember(vo);
+			/*else if (command != null && command.equals("delMember")) {
+				String id = request.getParameter("id");
+				dao.delMember(id);
+			}*/
+		}
+		
+		out.print("<html><body>");
+		out.print("<table border=1><tr align='center' bgcolor='lightgreen'>");
+		out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입일</td></tr>");
+		
+		for (int i = 0;i < list.size();i++) {
+			MemberVO memberVO = list.get(i);
+			String id = memberVO.getId();
+			String pwd = memberVO.getPwd();
+			String name = memberVO.getName();
+			String email = memberVO.getEmail();
+			Date joinDate = memberVO.getJoinDate();
+			out.print("<tr><td>" + id + "</td><td>" + pwd + "</td><td>" + name + "</td><td>" + email + "</td><td>" + joinDate + "</td></tr>");
+		}
+		out.print("</table></body></html>");
+		out.print("<a href='/Chapter07/MemberForm.html'>새 회원 등록하기</a>");
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doHandle(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doHandle(request, response);
+	}
+}
